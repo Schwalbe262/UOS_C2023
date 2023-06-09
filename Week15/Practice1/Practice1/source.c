c905d84c2bce3bd5c3bcc77a3fda3f2826e4a4d9
Schwalbe262 <r1jae262@uos.ac.kr>
2023-06-09 13:21:27 +0900

    week 15

---
+++ Week15/Practice1/Practice1/source.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+
+	FILE* fp1 = NULL;
+	fopen_s(&fp1, "data1.txt", "wt");
+
+	char temp[100];
+	
+	printf("입력을 종료하고 싶으면 END를 입력하시오\n");
+
+	while (1) {
+		printf("문자열 입력 : ");
+		gets(temp);
+		if (strcmp(temp, "END") == 0) break;
+		fputs(temp, fp1);
+	}
+	
+	fclose(fp1);
+}
+
+
---
+++ Week15/Practice2/Practice2/source.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    FILE* fp1 = NULL;
+    char* buf = NULL;
+    size_t bufSize = 0;
+
+    fopen_s(&fp1, "data1.txt", "rt");
+
+    if (fp1 == NULL) {
+        printf("파일 오픈 실패!");
+        return -1;
+    }
+
+    fseek(fp1, 0, SEEK_END);
+    bufSize = ftell(fp1);
+    fseek(fp1, 0, SEEK_SET);
+
+    buf = (char*)malloc((bufSize + 1) * sizeof(char));
+    if (buf == NULL) {
+        printf("메모리 할당 실패!");
+        fclose(fp1);
+        return -1;
+    }
+
+    memset(buf, 0, bufSize + 1);
+    fgets(buf, bufSize + 1, fp1);
+
+    printf("문자 수 : %d\n\n", bufSize);
+
+    printf("%s\n", buf);
+
+    free(buf);
+    fclose(fp1);
+
+    return 0;
+
+}
+
